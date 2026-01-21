98c87b8f82f808b5c017bafa7a8526018443d7b1
Clovis Inaka <youandustechno@gmail.com>
2026-01-20 21:55:00 -0600

    Third commit

--- app/src/main/java/com/clovis/meetme/MainActivity.kt
+++ app/src/main/java/com/clovis/meetme/MainActivity.kt
@@ -7,7 +7,8 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.clovis.meetme.databinding.ActivityMainBinding
+import com.clovis.second.R
+import com.clovis.second.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
